6ff7e3c5ce53193addadd1561517e826232c5227
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-15 10:52:43 +0100

    tests for withdraw and deposit functions

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,11 +1,30 @@
-describe('Account', function(){
-
+describe('Account', function() {
   var account;
-  beforeEach(function(){
+  beforeEach(function() {
     account = new Account();
   });
 
   it('has a default balance of £0', function() {
     expect(account.getBalance()).toEqual(0);
-  })
+  });
+
+  it('can be given a different default balance', function() {
+    account = new Account(100);
+    expect(account.getBalance()).toEqual(100);
+  });
+
+  describe('depositing', function() {
+    it('£10 increase account balance by £10', function() {
+      account.deposit(10);
+      expect(account.getBalance()).toEqual(10);
+    });
+  });
+
+  describe('withdrawing', function() {
+    it('£10 decreases account balance by £10', function() {
+      account.deposit(20);
+      account.withdraw(10);
+      expect(account.getBalance()).toEqual(10);
+    });
+  });
 });
--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,17 @@
-'use strict'
+"use strict";
 
-function Account(){
-  this.balance = 0;
+function Account(balance=0){
+  this.balance = balance;
 }
 
 Account.prototype.getBalance = function () {
   return this.balance;
+};
+
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+};
+
+Account.prototype.withdraw = function (amount) {
+  this.balance -= amount;
 };
