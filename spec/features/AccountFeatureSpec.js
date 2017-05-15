8bf59af6f81c49829af21d569366f19cc662d18e
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-15 12:47:30 +0100

    adds a transaction history array

--- spec/features/AccountFeatureSpec.js
+++ spec/features/AccountFeatureSpec.js
@@ -1,3 +1,22 @@
 describe('Account', function() {
-  
+  var account;
+  var transactions;
+
+  beforeEach(function() {
+    account = new Account();
+  });
+
+  describe('transaction history', function() {
+    it('add a transaction to history for deposits with date, type and amount', function() {
+      var todaysDate = account.transactions._date();
+      account.deposit(10);
+      expect(account.transactions.history).toEqual([{date: todaysDate, type: "credit", amount: 10}]);
+    });
+
+    it('add a transaction to history for withdrawals with date, type and amount', function() {
+      var todaysDate = account.transactions._date();
+      account.withdraw(5);
+      expect(account.transactions.history).toEqual([{date: todaysDate, type: "debit", amount: 5}]);
+    });
+  });
 });
--- src/Account.js
+++ src/Account.js
@@ -2,6 +2,7 @@
 
 function Account(balance=0){
   this.balance = balance;
+  this.transactions = new Transactions();
 }
 
 Account.prototype.getBalance = function () {
@@ -9,9 +10,11 @@ Account.prototype.getBalance = function () {
 };
 
 Account.prototype.deposit = function (amount) {
+  this.transactions.newTransaction("credit", amount);
   this.balance += amount;
 };
 
 Account.prototype.withdraw = function (amount) {
+  this.transactions.newTransaction("debit", amount);
   this.balance -= amount;
 };
